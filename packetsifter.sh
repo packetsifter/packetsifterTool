813365e656c7dd1115b09254cb4b25410021c0b0
packetsifter <75842371+packetsifter@users.noreply.github.com>
2021-01-08 07:23:01 -0600

    Update packetsifter.sh

--- packetsifter.sh
+++ packetsifter.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #user supplied pcap from cli
 pcap=$1
@@ -50,7 +50,7 @@ printf '\nStatistical data about HTTP conversations have been generated and are 
 
 #empty http_info.txt check
 httpcheck=$(cat http_info.txt | wc -l)
-if [[ $httpcheck == 36 ]]; then
+if [[ $httpcheck -eq 36 ]]; then
 	rm http_info.txt
 	printf '\nNo HTTP traffic found. Deleting arbitrary http_info.txt\n'
 fi
@@ -78,7 +78,7 @@ printf '\nhttp.pcap contains all conversations containing port 80,8080,8000\n'
 
 #empty http.pcap check
 httppcapcheck=$(tshark -r http.pcap | wc -l)
-if [[ $httppcapcheck == 0 ]]; then
+if [[ $httppcapcheck -eq 0 ]]; then
 	rm http.pcap
 	printf 'No HTTP traffic found. Deleting arbitrary http.pcap\n'
 fi
@@ -101,13 +101,40 @@ then
 	rm -rf ./httpObjects
 
 #HTTP Object Check
-	httpobjectcheck=$(tar -xvf httpObjects.tar.gz | wc -l)
-	if [[ $httpobjectcheck == 1 ]]; then
+	httpobjectcheck=$(tar -xzvf httpObjects.tar.gz | wc -l)
+	if [[ $httpobjectcheck -eq 1 ]]; then
 		rm httpObjects.tar.gz
 		rm -rf ./httpObjects
 		printf '\nNo HTTP Objects found. Deleting arbitrary httpObjects.tar.gz.\n'
+		rm -rf ./httpObjects
+	else
+#VirusTotal Integration HTTP
+		printf '\nWould you like to lookup exported HTTP objects using VirusTotal?\n**Warning** You must have ran the VTinitial.sh script to initialize PacketSifter with your VirusTotal API Key.\n'
+		printf '(Please supply Y for yes or N for no)\n'
+		read VTHTTPLookup
+			if [ $VTHTTPLookup == 'Y' ] | [ $VTHTTPLookup == 'y' ]
+			then
+				tar -xzf httpObjects.tar.gz
+				for VAL in $(ls ./httpObjects)
+				do
+					md5sum ./httpObjects/$VAL >> ./httpHashToObject.txt
+					cat httpHashToObject.txt | awk '{ print $1 }' | sort | uniq > lookup.txt
+				done
+
+				while read ARG
+				do
+					curl -s --request GET   --url https://www.virustotal.com/api/v3/files/$ARG  --header 'x-apikey: test' | jq '.data.attributes.md5, .data.attributes.last_analysis_stats, .error' | grep -E code\|[a-f0-9]{32}\|malicious\|undetected >> output.txt
+				done < lookup.txt
+				sed '/undetected/a\'$'\n'  output.txt > output2.txt 
+				sed '/message/a\'$'\n' output2.txt > httpVTResults.txt
+				rm -rf ./httpObjects
+				rm lookup.txt
+				rm output.txt
+				rm output2.txt
+			fi
 	fi
 	rm -rf ./httpObjects
+
 fi
 
 
@@ -121,17 +148,17 @@ tshark -nr $pcap -q -z smb2,srt >> SMBstatistics.txt 2>>errors.txt
 
 #smb statistics check
 smbstatscheck=$(cat SMBstatistics.txt | wc -l)
-if [[ $smbstatscheck == 18 ]]; then
+if [[ $smbstatscheck -eq 18 ]]; then
 	rm SMBstatistics.txt
 	printf '\nNo SMB traffic found. Deleting arbitrary SMBstatistics.txt\n'
 fi
 
-printf '\nsmb.pcap contains all conversations categorized by tshark dissectors as NBSS, SMB, or SMB2\n'
+printf 'smb.pcap contains all conversations categorized by tshark dissectors as NBSS, SMB, or SMB2\n'
 tshark -nr $pcap -Y nbss -w smb.pcap 2>>errors.txt
 
 #SMB pcap check
 smbpcapcheck=$(tshark -r smb.pcap | wc -l)
-if [[ $smbpcapcheck == 0 ]]; then
+if [[ $smbpcapcheck -eq 0 ]]; then
 	rm smb.pcap
 	printf '\nNo SMB traffic found. Deleting arbitrary smb.pcap.\n'
 fi
@@ -154,11 +181,36 @@ then
 	rm -rf ./smbObjects
 
 #SmbObject Check
-	smbobjectcheck=$(tar -xvf smbObjects.tar.gz | wc -l)
-	if [[ $smbobjectcheck == 1 ]]; then
+	smbobjectcheck=$(tar -xzvf smbObjects.tar.gz | wc -l)
+	if [[ $smbobjectcheck -eq 1 ]]; then
 		rm smbObjects.tar.gz
 		rm -rf ./smbObjects
 		printf '\nNo SMB Objects found. Deleting arbitrary smbObjects.tar.gz.\n'
+	else
+#VirusTotal Integration
+		printf '\nWould you like to lookup exported SMB objects using VirusTotal?\n**Warning** You must have ran the VTinitial.sh script to initialize PacketSifter with your VirusTotal API Key.\n'
+		printf '(Please supply Y for yes or N for no)\n'
+		read VTSMBLookup
+			if [ $VTSMBLookup == 'Y' ] | [ $VTSMBLookup == 'y' ]
+			then
+				tar -xzf smbObjects.tar.gz
+				for OBJ in $(ls ./smbObjects)
+				do
+					md5sum ./smbObjects/$OBJ >> ./smbHashToObject.txt
+					cat smbHashToObject.txt | awk '{ print $1 }' | sort | uniq > lookupsmb.txt
+				done
+
+				while read THE
+				do
+					curl -s --request GET   --url https://www.virustotal.com/api/v3/files/$THE  --header 'x-apikey: test' | jq '.data.attributes.md5, .data.attributes.last_analysis_stats, .error' | grep -E code\|[a-f0-9]{32}\|malicious\|undetected >> output.txt
+				done < lookupsmb.txt
+				sed '/undetected/a\'$'\n'  output.txt > output2.txt 
+				sed '/message/a\'$'\n' output2.txt > smbVTResults.txt
+				rm -rf ./smbObjects
+				rm lookupsmb.txt
+				rm output.txt
+				rm output2.txt
+			fi
 	fi
 	rm -rf ./smbObjects
 fi
@@ -174,7 +226,7 @@ printf '\ndns.pcap contains all conversations categorized by tshark dissectors a
 
 #DNS Pcap check
 dnspcapcheck=$(tshark -r dns.pcap | wc -l)
-if [[ $dnspcapcheck == 0 ]]; then
+if [[ $dnspcapcheck -eq 0 ]]; then
 	rm dns.pcap
 	printf 'No DNS traffic found. Deleting arbitrary dns.pcap\n'
 fi
@@ -185,7 +237,7 @@ printf '\nDNS A query/responses have been outputted to dnsARecords.txt\n'
 
 #DNS A record check
 dnsacheck=$(cat dnsARecords.txt | wc -l)
-if [[ $dnsacheck == 1 ]]; then
+if [[ $dnsacheck -eq 1 ]]; then
 	rm dnsARecords.txt
 	printf 'No DNS A records found. Deleting arbitrary dnsARecords.txt\n'
 fi
@@ -198,7 +250,7 @@ printf '\nDNS TXT query/responses have been outputted to dnsTXTRecords.txt. DNS 
 
 #DNS TXT record check
 dnstxtcheck=$(cat dnsTXTRecords.txt | wc -l)
-if [[ $dnstxtcheck == 1 ]]; then
+if [[ $dnstxtcheck -eq 1 ]]; then
 	rm dnsTXTRecords.txt
 	printf 'No DNS TXT records found. Deleting arbitrary dnsTXTRecords.txt\n'
 fi 
@@ -215,7 +267,7 @@ printf 'ftp.pcap contains all conversations categorized by tshark dissectors as 
 
 #FTP pcap check
 ftpcheck=$(tshark -r ftp.pcap | wc -l)
-if [[ $ftpcheck == 0 ]]; then
+if [[ $ftpcheck -eq 0 ]]; then
 	rm ftp.pcap
 	printf 'No FTP traffic found. Deleting arbitrary ftp.pcap\n'
 fi
@@ -224,7 +276,7 @@ fi
 
 #errors.txt removal
 errorcheck=$(cat errors.txt | wc -l)
-if [[ $errorcheck == 0 ]]; then
+if [[ $errorcheck -eq 0 ]]; then
 	rm errors.txt
 fi
 
@@ -233,4 +285,3 @@ fi
 #sifting done
 printf '\nPacket sifting complete! Thanks for using the tool.\n'
 printf '\nHappy hunting!\n'
-
